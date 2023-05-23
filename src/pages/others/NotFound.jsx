4a99ea892f7e00af422903f46d8a65448f591a65
Rahman Insani <rahmaninsani54@gmail.com>
2023-05-23 10:36:06 +0700

    update 404 not found

--- src/pages/others/NotFound.jsx
+++ src/pages/others/NotFound.jsx
@@ -1,7 +1,35 @@
 import React from "react";
+import { Container, Image } from "react-bootstrap";
+import { Link } from "react-router-dom";
+
+import error404 from "../../assets/images/error/404.png";
 
 const NotFound = () => {
-  return <h1>404 Not Found</h1>;
+  return (
+    <>
+      <div className="gradient">
+        <Container>
+          <Image src={error404} className="img-fluid mb-4 w-50" alt="" />
+          <h2 className="mb-0 mt-4 text-white">Halaman Tidak Ditemukan</h2>
+          <p className="mt-2 text-white">Halaman yang diminta tidak tersedia</p>
+          <Link className="btn bg-white text-primary d-inline-flex align-items-center" to="/">
+            Kembali
+          </Link>
+        </Container>
+        <div className="box">
+          <div className="c xl-circle">
+            <div className="c lg-circle">
+              <div className="c md-circle">
+                <div className="c sm-circle">
+                  <div className="c xs-circle"></div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default NotFound;
